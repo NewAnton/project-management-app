96924222ab8c4d5c3818cbea8c5efc507a2ec5cb
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-11-18 12:08:50 +0300

    add App.test.tsx

--- project-management-app/src/App.test.tsx
+++ project-management-app/src/App.test.tsx
@@ -1,9 +1,41 @@
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { App } from './App';
+import { Main } from 'pages/Main/Main';
+import { BoardList } from 'pages/BoardList/BoardList';
+import { Board } from 'pages/Board/Board';
+import { ErrorPage404 } from 'pages/404ErrorPage/404ErrorPage';
+import { BrowserRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Router', () => {
+  test('should render App', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(screen.getByText(/my app/i)).toBeInTheDocument();
+  });
+  test('should render loading', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
+    expect(screen.getByTestId(/load-container/i)).toBeInTheDocument();
+  });
+  test('should render error page if the path is wrong', () => {
+    render(
+      <MemoryRouter initialEntries={['/wrong-route']}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/board-list" element={<BoardList />} />
+          <Route path="/board" element={<Board />} />
+          <Route path="*" element={<ErrorPage404 />} />
+        </Routes>
+      </MemoryRouter>
+    );
+    expect(screen.getByText(/This page does not exist/i)).toBeInTheDocument();
+  });
 });
