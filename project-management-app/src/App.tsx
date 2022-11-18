ab214b7927f926a83eb50b1ffccde651a3431792
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-11-18 11:40:13 +0300

    feat: create main page

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -2,7 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 
 import { Navigation } from './components/Navigation/Navigation';
-
+import { Main } from 'pages/Main/Main';
 import './App.scss';
 
 export function App() {
@@ -10,11 +10,11 @@ export function App() {
     <>
       <Navigation />
       <Routes>
-        {/* <Route path="/" element={<Main />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/forms" element={<FormsPage />} />
-          <Route path="/card" element={<Modal card={card} />} />
-          <Route path="*" element={<ErrorPage404 />} /> */}
+        <Route path="/" element={<Main />} />
+        {/* <Route path="/about" element={<About />} />
+        <Route path="/forms" element={<FormsPage />} />
+        <Route path="/card" element={<Modal card={card} />} />
+        <Route path="*" element={<ErrorPage404 />} /> */}
       </Routes>
     </>
   );
