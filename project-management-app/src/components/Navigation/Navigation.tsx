efd6213ad269117664421868222d36ed9135be25
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-11-20 00:26:57 +0300

    fix: change header

--- project-management-app/src/components/Navigation/Navigation.tsx
+++ project-management-app/src/components/Navigation/Navigation.tsx
@@ -1,27 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlus,
+  faUser,
+  faListSquares,
+  faGlobe,
+  faRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 
 import './Navigation.scss';
 
 export function Navigation() {
+  const [navBarTheme, setnavBarTheme] = useState('header__navbar');
+
+  const changeNavBarTheme = () => {
+    window.scrollY >= 80
+      ? setnavBarTheme('header__navbar navbar-scroll')
+      : setnavBarTheme('header__navbar');
+  };
+
+  window.addEventListener('scroll', changeNavBarTheme);
+
   return (
-    <Navbar bg="light" expand="lg" className="mb-4">
+    <Navbar collapseOnSelect expand="lg" variant="dark" className={navBarTheme}>
       <Container>
-        <Navbar.Brand href="/">Project Management App</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-2 my-lg-0 nav__container" navbarScroll>
-            <Nav.Link as={Link} to="/">
-              Main
+        <Navbar.Brand className="font-weight-bold" href="/">
+          Project Management App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Nav className="me-auto mr-5">
+            <Nav.Link className="navbar__link" as={Link} to="/board">
+              <FontAwesomeIcon className="element__star mr-1" icon={faPlus} size="xs" />
+              New Board
             </Nav.Link>
-            <Nav.Link as={Link} to="/board-list">
-              Board list
+            <Nav.Link className="navbar__link" as={Link} to="/board-list">
+              <FontAwesomeIcon className="element__star mr-1" icon={faListSquares} size="xs" />
+              Boards
             </Nav.Link>
-            <Nav.Link as={Link} to="/board">
-              Board
+            <Nav.Link className="navbar__link" as={Link} to="/profile">
+              <FontAwesomeIcon className="element__star mr-1" icon={faUser} size="xs" />
+              Edit Profile
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link className="navbar__link" as={Link} to="/bo">
+              <FontAwesomeIcon className="element__star mr-1" icon={faGlobe} size="xs" />
+              En
+            </Nav.Link>
+            <Nav.Link className="navbar__link" as={Link} to="/bo">
+              <FontAwesomeIcon className="element__star mr-1" icon={faRightFromBracket} size="xs" />
+              Sign out
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
--- project-management-app/src/pages/Main/Main.tsx
+++ project-management-app/src/pages/Main/Main.tsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
 export function Main() {
-  return <h1>Main Page</h1>;
+  return (
+    <>
+      <h1>Main Page</h1>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+      <h2>Main Page</h2>
+    </>
+  );
 }
