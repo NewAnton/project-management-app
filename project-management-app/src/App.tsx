6d1cf9bfdff1d39cf567b27ccfc6e2e803c7352f
Alexej-Ilyutik <alexej.ilyutik@gmail.com>
2022-11-21 10:44:52 +0300

    feat: create Profile page

--- project-management-app/src/App.tsx
+++ project-management-app/src/App.tsx
@@ -6,6 +6,7 @@ import { Main } from 'pages/Main/Main';
 import { BoardList } from 'pages/BoardList/BoardList';
 import { Board } from 'pages/Board/Board';
 import { NewBoard } from 'pages/NewBoard/NewBoard';
+import { Profile } from 'pages/Profile/Profile';
 import { ErrorPage404 } from 'pages/404ErrorPage/404ErrorPage';
 import { Footer } from 'components/Footer/Footer';
 
@@ -23,6 +24,7 @@ export function App() {
           <Route path="/board-list" element={<BoardList />} />
           <Route path="/new-board" element={<NewBoard />} />
           <Route path="/board" element={<Board />} />
+          <Route path="/profile" element={<Profile />} />
           <Route path="*" element={<ErrorPage404 />} />
         </Routes>
       </main>
--- project-management-app/src/pages/BoardList/BoardList.tsx
+++ project-management-app/src/pages/BoardList/BoardList.tsx
@@ -10,7 +10,7 @@ import './BoardList.scss';
 export function BoardList() {
   return (
     <Container>
-      <h2 className="board-list__title">Boards</h2>
+      <h2 className="main__title">Boards</h2>
       <div className="board-list__container">
         {Array.from({ length: 9 }).map((_, idx) => (
           <Nav.Link className="board-list__link" key={idx} as={Link} to="/board">
