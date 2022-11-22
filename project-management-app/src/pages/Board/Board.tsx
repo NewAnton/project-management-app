169fab9792856f84689d3930310a524c12684ea3
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-11-22 12:56:57 +0300

    fix: change board__container styles

--- project-management-app/src/pages/Board/Board.tsx
+++ project-management-app/src/pages/Board/Board.tsx
@@ -9,17 +9,17 @@ import './Board.scss';
 
 export function Board() {
   return (
-    <>
+    <div className="board__wrapper container-fluid">
       <Container>
         <h2 className="main__title">Board</h2>
       </Container>
-      <div className="board__container">
-        {Array.from({ length: 8 }).map((_, idx) => (
+      <div className="board__container row flex-row flex-nowrap mt-4 pb-4 pt-2">
+        {Array.from({ length: 6 }).map((_, idx) => (
           <Nav.Link className="board__link" key={idx} as={Link} to="/task">
             <PrevCard />
           </Nav.Link>
         ))}
       </div>
-    </>
+    </div>
   );
 }
