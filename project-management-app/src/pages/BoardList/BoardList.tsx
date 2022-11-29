670a56ecba5f88f82350eac19088ba46284dfbd1
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-11-29 13:16:25 +0300

    feat: add Loading.tsx

--- project-management-app/src/pages/BoardList/BoardList.tsx
+++ project-management-app/src/pages/BoardList/BoardList.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
-import { useGetAllBoardsQuery } from 'services/kanbanApiBoards';
 
+import { useGetAllBoardsQuery } from 'services/kanbanApiBoards';
 import { PrevBoard } from 'components/PrevBoard/PrevBoard';
+import { ErrorMessage } from 'components/ErrorMessage/ErrorMessage';
+import { Loading } from 'components/Loading/Loading';
 
 import './BoardList.scss';
-import { ErrorMessage } from 'components/ErrorMessage/ErrorMessage';
 
 export function BoardList() {
   const { isLoading, isError, data } = useGetAllBoardsQuery();
@@ -17,13 +18,14 @@ export function BoardList() {
     <Container>
       <h2 className="main__title">Boards List</h2>
       {isError && <ErrorMessage message="Something went wrong..." />}
-      {/* <div className="board-list__container">
+      <div className="board-list__container">
+        {isLoading && <Loading />}
         {Array.from({ length: 9 }).map((_, idx) => (
           <Nav.Link className="board-list__link" key={idx} as={Link} to="/board">
             <PrevBoard />
           </Nav.Link>
         ))}
-      </div> */}
+      </div>
     </Container>
   );
 }
