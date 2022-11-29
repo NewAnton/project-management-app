7ba4cb75fcf47fbfe46976e77fbb22d72cd92470
Alexej Ilyutik <alexej.ilyutik@gmail.com>
2022-11-29 16:48:36 +0300

    feat: add Boards list from backend

--- project-management-app/src/components/Loading/Loading.tsx
+++ project-management-app/src/components/Loading/Loading.tsx
@@ -3,7 +3,10 @@ import Spinner from 'react-bootstrap/Spinner';
 
 export function Loading() {
   return (
-    <div className="load__container d-flex align-center" data-testid="load-container">
+    <div
+      className="load__container d-flex align-center position-absolute"
+      data-testid="load-container"
+    >
       <Spinner animation="border" variant="warning" />
       <div className="load__text pl-2">Loading...</div>
     </div>
--- project-management-app/src/components/PrevBoard/PrevBoard.tsx
+++ project-management-app/src/components/PrevBoard/PrevBoard.tsx
@@ -5,11 +5,13 @@ import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 import './PrevBoard.scss';
 
-// interface IPrevCardProps {
-//   getCurrentCard: () => void;
-// }
+interface IPrevBoardProps {
+  title: string;
+  description: string;
+  // getCurrentCard: () => void;
+}
 
-export function PrevBoard() {
+export function PrevBoard(props: IPrevBoardProps) {
   return (
     <Card bg="Light" text="dark" className="prevboard mb-4">
       <Card.Header className="d-flex justify-content-between">
@@ -23,11 +25,8 @@ export function PrevBoard() {
         </div>
       </Card.Header>
       <Card.Body>
-        <Card.Title> Card Title </Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card
-          content.
-        </Card.Text>
+        <Card.Title> {props.title} </Card.Title>
+        <Card.Text>{props.description}</Card.Text>
       </Card.Body>
     </Card>
   );
