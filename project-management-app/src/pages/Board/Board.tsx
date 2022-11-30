d770ea40cdef3f712ccc1659354e13c164ba529a
EJ252 <w660@live.ru>
2022-12-01 00:06:42 +0300

    fix: revert changes in Board.tsx

--- project-management-app/src/pages/Board/Board.tsx
+++ project-management-app/src/pages/Board/Board.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 import { Card } from 'components/Card/Card';
 
 import './Board.scss';
-import { FormValues } from 'components/NewBoard/NewBoard';
 
-export function Board(board: FormValues) {
+export function Board() {
   return (
     <div className="board__wrapper container-fluid">
       <Container>
-        <h2 className="main__title">{board.name}</h2>
+        <h2 className="main__title">Board</h2>
       </Container>
       <div className="board__container row flex-row flex-nowrap mt-4 pb-4 pt-2">
-        {Array.from({ length: 8 }).map((_, idx) => (
+        {Array.from({ length: 6 }).map((_, idx) => (
           <Card key={idx} />
         ))}
-        <div className="board__card card-btn">
-          <div className="card-btn__title">
-            <FontAwesomeIcon className="mr-1" icon={faPlus} size="xs" />
-            Add Card
-          </div>
-        </div>
       </div>
     </div>
   );
